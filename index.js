e2216e19e017b241be176faa0eb036da215f6b5d
lucas-saldanha <lk.saldanha@gmail.com>
2022-11-20 21:42:27 +0100

    refactoring

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ function requestData() {
 // var clock = setInterval(requestData, 5000); // Get data every five second
 
 socket.on("getHeaterState", function(data) {
-  // Update the web page
   if (data == 1) {
     document.getElementById("heaterStatus").innerHTML = "Status: ON";
   } else {
@@ -22,31 +21,15 @@ socket.on("getWindowState", function (data) {
 socket.on("getLightLevel", function (data) {
   document.querySelector("#lightStatus").innerHTML = `Current Level: ${data}`;
 });
-//   var slider = document.getElementById("lightSlider");
-//   var output = document.getElementById("lightValue");
-
-//   // output.innerHTML = slider.value;
-
-//   output.onchange = function() {
-//     slider.value = this.value;
-//   }
-//   output.innerHTML = data;
-
-//   // slider.oninput = function() {
-//   //   output.innerHTML = this.value;
-//   // }
-// });
 
 socket.on("currentData", function (data) {
   console.log(data);
 });
 
-// Changes the heater state
 function changeHeaterState(state) {
     socket.emit("changeHeaterState", state);
 }
 
-// Changes the window state
 function changeWindowState(state) {
   if (state == 1) {
     socket.emit("changeWindowState", '{"state":"open"}');
@@ -70,60 +53,9 @@ document.getElementById("lightSlider").addEventListener("input", function() {
 });
 
 
-// send the light level to the server
+// listen to submit and send the new light level to the server
 document.getElementById("setNewLight").addEventListener("click", function() {
   var value = document.getElementById("lightSlider").value;
   console.log("Light level sent to server: " + value);
   socket.emit("changeLightLevel", value);
 });
-
-
-// wait until the DOM is loaded, then run the function
-// document.addEventListener('DOMContentLoaded', function() {
-//   document.querySelectorAll('button').forEach(function(button) {
-//     button.onclick = function() {
-//       console.log(button.id);
-//       switch (button.id) {
-//         case 'heaterOn':
-//           changeHeaterState(1);
-//           break;
-//         case 'heaterOff':
-//           changeHeaterState(0);
-//           break;
-//         case 'windowOpen':
-//           changeWindowState(1);
-//           break;
-//         case 'windowClose':
-//           changeWindowState(0);
-//           break;
-//         default:
-//           console.log('Error');
-//       }
-//     };
-//   }
-// )});
-
-// // // wait until the DOM is loaded, then run the function
-// document.addEventListener('DOMContentLoaded', () => {
-//   document.querySelectorAll('button').forEach(button => {
-//     button.onchange = () => {
-//       console.log(button.id);
-//       switch (button.id) {
-//         case 'heaterOn':
-//           changeHeaterState(1);
-//           break;
-//         case 'heaterOff':
-//           changeHeaterState(0);
-//           break;
-//         case 'windowOpen':
-//           changeWindowState(1);
-//           break;
-//         case 'windowClose':
-//           changeWindowState(0);
-//           break;
-//         default:
-//           console.log('Error');
-//       }
-//     }
-//   });
-// });
