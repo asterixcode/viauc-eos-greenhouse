8f7b0c89f18311760a2be5142be8f4a44a6ed4bc
Lucas Krause Saldanha <62264294+asterixcode@users.noreply.github.com>
2022-11-21 11:42:00 +0100

    changed heater to work with led script (#4)
    * changed heater to work with led script
    
    * small comment into the build script

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 #command to compile the c++ code to read from hih8120Sensor
 g++ I2CDevice.h I2CDevice.cpp hih8120Sensor.h hih8120Sensor.cpp main.cpp -o readTempHum
+
+# set read write execute permissions to the output file
+chmod a+rwx readTempHum
---
+++ control-leds.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+if [ ! "on" == "$1" ] && [ ! "off" == "$1" ]
+  then 
+    echo
+    echo "ERROR: Invalid second argument. Valid values: on or off."
+    echo
+    exit 1
+fi
+
+if [ "$1" == "on" ]
+  then
+    cd /sys/class/leds/beaglebone\:green\:usr0
+    echo none > trigger
+    echo 1 > brightness
+
+    cd /sys/class/leds/beaglebone\:green\:usr1
+    echo none > trigger
+    echo 1 > brightness
+
+    cd /sys/class/leds/beaglebone\:green\:usr2
+    echo none > trigger
+    echo 1 > brightness
+
+    cd /sys/class/leds/beaglebone\:green\:usr3
+    echo none > trigger
+    echo 1 > brightness
+    
+elif [ "$1" == "off" ]
+  then
+    cd /sys/class/leds/beaglebone\:green\:usr0
+    echo none > trigger
+    echo 0 > brightness
+
+    cd /sys/class/leds/beaglebone\:green\:usr1
+    echo none > trigger
+    echo 0 > brightness
+
+    cd /sys/class/leds/beaglebone\:green\:usr2
+    echo none > trigger
+    echo 0 > brightness
+
+    cd /sys/class/leds/beaglebone\:green\:usr3
+    echo none > trigger
+    echo 0 > brightness
+fi
