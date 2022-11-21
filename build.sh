c2a92bed89ecdce9f881fc385d08cf8dfea137be
lucas-saldanha <lk.saldanha@gmail.com>
2022-11-21 11:38:56 +0100

    small comment into the build script

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 #command to compile the c++ code to read from hih8120Sensor
 g++ I2CDevice.h I2CDevice.cpp hih8120Sensor.h hih8120Sensor.cpp main.cpp -o readTempHum
 
+# set read write execute permissions to the output file
 chmod a+rwx readTempHum
