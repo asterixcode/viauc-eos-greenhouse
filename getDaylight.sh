5d785a50155d69bf5c0e16c56f48a2e2428e4f85
Michal Pupák <58641428+Pupendo@users.noreply.github.com>
2022-11-25 11:53:34 +0100

    Update getDaylight.sh

--- getDaylight.sh
+++ getDaylight.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# raw volatage levels can be read from the following path according to the book
+# raw voltage levels can be read from the following path according to the book
 cd /sys/bus/iio/devices/iio:device0
 cat in_voltage0_raw
