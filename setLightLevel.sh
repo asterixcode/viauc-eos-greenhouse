e5e46c1c13e3078accf36c8ba2e9288c91b080db
Michal Pupák <58641428+Pupendo@users.noreply.github.com>
2022-11-27 13:33:21 +0100

    Update setLightLevel.sh

--- setLightLevel.sh
+++ setLightLevel.sh
@@ -5,7 +5,10 @@
 
 config-pin P9_22 pwm
 sh -c "echo 1 > export"
+
+#calculate value to pass to duty_cycle, 200000 represents 1% intensity, $1 is desired intensity
 cycle=$((200000 * $1))
+#go to correct directory and update period, duty_cycle, and enable to update intensity 
 cd /sys/class/pwm/pwmchip1/
 cd pwm-1\:0
 sh -c "echo 20000000 > period"
