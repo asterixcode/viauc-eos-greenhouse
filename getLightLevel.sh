b4086881fc5dc1e4b81d249397de776293cc42a6
Michal Pupák <58641428+Pupendo@users.noreply.github.com>
2022-11-27 13:25:16 +0100

    Update getLightLevel.sh

--- getLightLevel.sh
+++ getLightLevel.sh
@@ -3,8 +3,11 @@
 # Author: Michal Pupak - 299128
 # Date: 27.11.22
 
+#change to proper directory
 cd /sys/class/pwm/pwmchip1/
 cd pwm-1\:0
+
+#read value from duty_cycle and calculate the light level
 value=$(<duty_cycle)
 result=$(($value/200000))
 echo $result
