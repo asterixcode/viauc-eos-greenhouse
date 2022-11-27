80276867e9ebb734e4f99fe71da0c8e4d82c373f
Michal Pupák <58641428+Pupendo@users.noreply.github.com>
2022-11-27 13:14:58 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# Author: Michal Pupak - 299128, Lucas Krause Saldanha – 300709
+# Date: 27.11.22
+
+
 #command to compile the c++ code to read from hih8120Sensor
 g++ I2CDevice.h I2CDevice.cpp hih8120Sensor.h hih8120Sensor.cpp main.cpp -o readTempHum
 
